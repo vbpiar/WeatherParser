d41fee3025707c909478cc88a9c1d283643e6f58
Taras <vbpiar@gmail.com>
2019-12-08 11:57:24 +0200

    Change show method

--- weather/app/Http/Controllers/FeedbackController.php
+++ weather/app/Http/Controllers/FeedbackController.php
@@ -13,22 +13,19 @@ use Illuminate\Support\Facades\Validator;
 class FeedbackController extends Controller
 {
 
-    public function create(Request $request)
+    public function store(Request $request)
     {
-
           $request->validate([
                 'name' => 'string|max:255',
                 'email' => 'string|email|max:255|unique:users',
                 'text' => 'required|string|max:255',
             ]);
-
                 // create new Feedback and store it
             $feedback = new Feedback();
             $feedback->name = $request['name'];
             $feedback->email = $request['email'];
             $feedback->text = $request['text'];
-            Auth::check() ? $feedback->user_id = auth()->user()->id : $feedback->user_id = null; // create eloquent id if user register
-
+            Auth::check() ? $feedback->user_id = auth()->user()->id : $feedback->user_id = null; // create associate id if user register
             $feedback->save();
 
             return redirect()->route('feedback.show');
@@ -37,8 +34,13 @@ class FeedbackController extends Controller
 
     public function show()
     {
-        $feedback = Feedback::all();
+        $feedback = Feedback::with('user')->get();
         return view('feedback.show',['feedback'=>$feedback]);
+    }
+
+    public function create()
+    {
+        return view('feedback.create');
     }
 
 
